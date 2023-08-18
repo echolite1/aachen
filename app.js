ce036742fef74fce7c446c2d30e25c6c61d5d016
Ernest Hunko <echolite221@gmail.com>
2023-08-18 14:35:43 +0200

    extended functionalities
    Added RWTH scan
    Added type names

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ today = dd + '-' + mm + '-' + yyyy;
 const constantLink = 'https://termine.staedteregion-aachen.de/auslaenderamt/';
 const defaultTime = 1000;
 const multiplier = 4;
-var people = [198, 201, 202]; //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
-var category = 115;
+// type variables
+const regular = {category: 115, buttons: [198, 201, 202], name: "Regular Person"};
+const studentFH = {category: 117, buttons: [193], name: "FH Student"};//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+const studentRWTH = {category: 114, buttons: [191], name: "RWTH Student"};
 // other functions
 function delay(time) {
     return new Promise(function(resolve) { setTimeout(resolve, time) });
@@ -25,10 +27,11 @@ async function findAppointment(){
     console.clear();
     console.log('- - - - - NEW SCAN ' + today + ' - - - - -');
 
-    await People(category, people);
-    //await FHStudents();
+    //await personType(regular.category, regular.buttons, regular.name);
+    //await personType(studentFH.category, studentFH.buttons, studentFH.name);
+    await personType(studentRWTH.category, studentRWTH.buttons, studentRWTH.name);
 
-    async function People(categoryID, peopleType) {
+    async function personType(categoryID, buttonType, personName) {
             var link = constantLink;
             await page.goto(link);
             await delay(defaultTime);
@@ -39,13 +42,13 @@ async function findAppointment(){
             console.log('p1 done');
             await delay(defaultTime);
 
-            [secondPage_Aufenthalt] = await page.$x('//*[@id="header_concerns_accordion-'+categoryID+'\"]');
+            [secondPage_Aufenthalt] = await page.$x('//*[@id="header_concerns_accordion-' + categoryID + '\"]');
             await secondPage_Aufenthalt.evaluate(secondPage_Aufenthalt => secondPage_Aufenthalt.click());
             console.log('p2 dropdown');
             await delay(defaultTime);
             //new team
-            for(const element of peopleType){
-                [secondPage_T1Plus] = await page.$x('//*[@id="button-plus-'+element+'\"]');
+            for(const element of buttonType){
+                [secondPage_T1Plus] = await page.$x('//*[@id="button-plus-' + element + '\"]');
                 await secondPage_T1Plus.evaluate(secondPage_T1Plus => secondPage_T1Plus.click());
                 console.log('p2 team selected');
                 await delay(defaultTime);
@@ -64,7 +67,8 @@ async function findAppointment(){
                 await thirdPage_Location.evaluate(thirdPage_Location => thirdPage_Location.click());
                 console.log('p3 location');
                 await delay(defaultTime * multiplier);
-    
+                
+                console.log("\n" + personName);
                 try {
                     await page.waitForSelector("#inhalt > div.info > p > strong > span", { timeout: 500 });
     
@@ -91,7 +95,7 @@ async function findAppointment(){
     
                 [thirdPage_Back] = await page.$x('//*[@id="zurueck"]');
                 await thirdPage_Back.evaluate(thirdPage_Back => thirdPage_Back.click());
-                console.log('p3 Zurück zur Standortauswahl');
+                console.log('\np3 Zurück zur Standortauswahl');
                 await delay(defaultTime);
     
                 [secondPage_Back] = await page.$x('//*[@id="zurueck"]');
@@ -99,136 +103,14 @@ async function findAppointment(){
                 console.log('p2 Zurück');
                 await delay(defaultTime);
     
-                [secondPage_T1Minus] = await page.$x('//*[@id="button-minus-'+element+'\"]');
+                [secondPage_T1Minus] = await page.$x('//*[@id="button-minus-' + element + '\"]');
                 await secondPage_T1Minus.evaluate(secondPage_T1Minus => secondPage_T1Minus.click());
                 console.log('p2 no team');
                 await delay(defaultTime);
             }
-
-            // new team
-            /*
-            [secondPage_T2Plus] = await page.$x('//*[@id="button-plus-201"]');
-            await secondPage_T2Plus.evaluate(secondPage_T2Plus => secondPage_T2Plus.click());
-            console.log('p2 team 2');
-            await delay(defaultTime);
-
-            [secondPage_Proceed] = await page.$x('//*[@id="WeiterButton"]');
-            await secondPage_Proceed.evaluate(secondPage_Proceed => secondPage_Proceed.click());
-            console.log('p2 proceed');
-            await delay(defaultTime);
-
-            [secondPage_Popup] = await page.$x('//*[@id="OKButton"]'); // might be skippable
-            await secondPage_Popup.evaluate(secondPage_Popup => secondPage_Popup.click());
-            console.log('p2 popup');
-            await delay(defaultTime * multiplier);
-
-            [thirdPage_Location] = await page.$x('//*[@id="ui-id-2"]/form/table/tbody/tr[5]/td/input');
-            await thirdPage_Location.evaluate(thirdPage_Location => thirdPage_Location.click());
-            console.log('p3 location');
-            await delay(defaultTime * multiplier);
-
-            [thirdPage_Result] = await page.$x('//*[@id="inhalt"]/div[2]/h2');
-            result = await thirdPage_Result.getProperty('textContent');
-            noAppointments = await result.jsonValue();
-            console.log('p3 no appointments');
-
-            console.log(noAppointments);
-
-            [thirdPage_Back] = await page.$x('//*[@id="zurueck"]');
-            await thirdPage_Back.evaluate(thirdPage_Back => thirdPage_Back.click());
-            console.log('p3 Zurück zur Standortauswahl');
-            await delay(defaultTime);
-
-            [secondPage_Back] = await page.$x('//*[@id="zurueck"]');
-            await secondPage_Back.evaluate(secondPage_Back => secondPage_Back.click());
-            console.log('p2 Zurück');
-            await delay(defaultTime);
-
-            [secondPage_T2Minus] = await page.$x('//*[@id="button-minus-201"]');
-            await secondPage_T2Minus.evaluate(secondPage_T2Minus => secondPage_T2Minus.click());
-            console.log('p2 no team');
-            await delay(defaultTime);
-            //new team
-            [secondPage_T3Plus] = await page.$x('//*[@id="button-plus-202"]');
-            await secondPage_T3Plus.evaluate(secondPage_T3Plus => secondPage_T3Plus.click());
-            console.log('p2 team 3');
-            await delay(defaultTime);
-
-            [secondPage_Proceed] = await page.$x('//*[@id="WeiterButton"]');
-            await secondPage_Proceed.evaluate(secondPage_Proceed => secondPage_Proceed.click());
-            console.log('p2 proceed');
-            await delay(defaultTime);
-
-            [secondPage_Popup] = await page.$x('//*[@id="OKButton"]'); // might be skippable
-            await secondPage_Popup.evaluate(secondPage_Popup => secondPage_Popup.click());
-            console.log('p2 popup');
-            await delay(defaultTime * multiplier);
-
-            [thirdPage_Location] = await page.$x('//*[@id="ui-id-2"]/form/table/tbody/tr[5]/td/input');
-            await thirdPage_Location.evaluate(thirdPage_Location => thirdPage_Location.click());
-            console.log('p3 location');
-            await delay(defaultTime * multiplier);
-
-            [thirdPage_Result] = await page.$x('//*[@id="inhalt"]/div[2]/h2');
-            result = await thirdPage_Result.getProperty('textContent');
-            noAppointments = await result.jsonValue();
-            console.log('p3 no appointments');
-
-            console.log(noAppointments);
-            */
         
         return;
     }
-    async function FHStudents() {
-        var link = constantLink;
-        await page.goto(link);
-        await delay(defaultTime);
-        
-        // pressing buttons section
-        [mainPage_FirstOption] = await page.$x('//*[@id="buttonfunktionseinheit-1"]');
-        await mainPage_FirstOption.evaluate(mainPage_FirstOption => mainPage_FirstOption.click());
-        console.log('p1 done');
-        await delay(defaultTime);
-
-        [secondPage_Aufenthalt] = await page.$x('//*[@id="header_concerns_accordion-117"]');
-        await secondPage_Aufenthalt.evaluate(secondPage_Aufenthalt => secondPage_Aufenthalt.click());
-        console.log('p2 dropdown');
-        await delay(defaultTime);
-        //new team
-        [secondPage_T1Plus] = await page.$x('//*[@id="button-plus-193"]');
-        await secondPage_T1Plus.evaluate(secondPage_T1Plus => secondPage_T1Plus.click());
-        console.log('p2 Studenten');
-        await delay(defaultTime);
-
-        [secondPage_Proceed] = await page.$x('//*[@id="WeiterButton"]');
-        await secondPage_Proceed.evaluate(secondPage_Proceed => secondPage_Proceed.click());
-        console.log('p2 proceed');
-        await delay(defaultTime);
-
-        [secondPage_Popup] = await page.$x('//*[@id="OKButton"]'); // might be skippable
-        await secondPage_Popup.evaluate(secondPage_Popup => secondPage_Popup.click());
-        console.log('p2 popup');
-        await delay(defaultTime * multiplier);
-
-        [thirdPage_Location] = await page.$x('//*[@id="ui-id-2"]/form/table/tbody/tr[5]/td/input');
-        await thirdPage_Location.evaluate(thirdPage_Location => thirdPage_Location.click());
-        console.log('p3 location');
-        await delay(defaultTime * multiplier);
-
-        for(let i = 1; i < 10; i++){
-            try{
-                [thirdPage_Result] = await page.$x('//*[@id="ui-id-'+i+'\"]');
-                result = await thirdPage_Result.getProperty('textContent');
-                resultText = await result.jsonValue();
-                console.log(resultText);
-            }
-            catch(error){
-                console.error('Error: ' + error);
-            }
-        }
-            
-    return;
-}
     browser.close();
 }
 // ************************************** MAIN ******************************************************
